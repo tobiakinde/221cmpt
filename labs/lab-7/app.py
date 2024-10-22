21f22cb98151eba25036464fe635efce8e3c17c1
calista phippen <profphip42@gmail.coom>
2024-10-22 14:39:11 -0400

    lab 7

--- labs/lab-7/app.py
+++ labs/lab-7/app.py
@@ -12,8 +12,26 @@ from db.schema.user import User
 def index():
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+
+        for key, value in request.form.items():
+            print(f'{key}: {value}')
+
+        # create insert query
+        insert_stmt = insert(User).values(request.form)
+
+        with app.app_context():
+            # execute insert query
+            db.session.execute(insert_stmt)
+
+            # commit the changes to the db
+            db.session.commit()
+            
+        # redirect to the home page
+        return redirect(url_for('index'))
+
     return render_template('signup.html')
 
 @app.route('/login')
@@ -23,13 +41,14 @@ def login():
 @app.route('/users')
 def users():
     with app.app_context():
-        # select users where the first name is Calista
+        # select users with the first name Calista
         # stmt = select(User).where(User.FirstName == "Calista")
 
         # select all users
         stmt = select(User)
-        all_users = db.session.execute(stmt)
 
+        all_users = db.session.execute(stmt)
+        
         return render_template('users.html', users=all_users)
     
     return render_template('users.html')
